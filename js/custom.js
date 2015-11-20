e5983e2c643190c711071dd070268f49b3fb96e9
Katie O'Leary <olearyk17@gmail.com>
2015-11-20 12:55:46 -0500

    finished

--- js/custom.js
+++ js/custom.js
@@ -3,8 +3,37 @@
 $(document).ready(function(){
 
 
+
+function scrollWindow()
+{
+window.scrollTo(0,0);
+}
+  
+  // $(document).scrollTop(0);
+  
+
     $('.color-overlay').hover(function(){
         $('h3', $(this)).slideToggle(200, 'linear').display(200, 'linear');
     });
+
+
+  $("#teamButton").click(function(){
+      $("#katie").fadeIn("slow");
+    });
+
+   $("#teamButton").click(function(){
+      $("#hannah").fadeIn("slow");
+    });
+
+  
+ 
+
+
+
+    // var slideW = $('#katie').width();
+
+    // $("#teamButton").click(function(){
+    //   $("#katie").animate({scrollRight: slideW }, 600);
+    // });
 
 });
